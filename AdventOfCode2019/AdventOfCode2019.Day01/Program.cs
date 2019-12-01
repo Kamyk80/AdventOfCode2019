c4e900183112af4ccd55c625143884dd1d2ab92d
Kamyk80 <kamyk80@outlook.com>
2019-12-01 12:00:17 +0100

    Day 01

--- AdventOfCode2019/AdventOfCode2019.Day01/Program.cs
+++ AdventOfCode2019/AdventOfCode2019.Day01/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2019.Day01
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var modules = File.ReadAllLines("input.txt")
+                .Select(int.Parse)
+                .ToList();
+
+            var modulesFuel = modules
+                .Sum(PartialFuel);
+
+            var totalFuel = modules
+                .Sum(ModuleFuel);
+
+            Console.WriteLine(modulesFuel);
+            Console.WriteLine(totalFuel);
+            Console.ReadKey(true);
+        }
+
+        private static int PartialFuel(int mass)
+        {
+            return mass / 3 - 2;
+        }
+
+        private static int ModuleFuel(int mass)
+        {
+            var fuel = 0;
+            while (mass > 0)
+            {
+                mass = mass / 3 - 2;
+                fuel += mass > 0 ? mass : 0;
+            }
+
+            return fuel;
         }
     }
 }

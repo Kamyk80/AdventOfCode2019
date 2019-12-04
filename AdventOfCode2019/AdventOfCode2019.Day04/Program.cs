54079e296c114189c8189f33b01ebc4c6a517db3
Kamyk80 <kamyk80@outlook.com>
2019-12-04 10:35:01 +0100

    Day 04

---
+++ AdventOfCode2019/AdventOfCode2019.Day04/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2019.Day04
+{
+    public class Program
+    {
+        private const int PasswordMin = 273025;
+        private const int PasswordMax = 767253;
+
+        private static void Main()
+        {
+            var criteria1Met = Enumerable.Range(PasswordMin, PasswordMax - PasswordMin + 1)
+                .Count(p => Criteria1Met(p.ToString()));
+            var criteria2Met = Enumerable.Range(PasswordMin, PasswordMax - PasswordMin + 1)
+                .Count(p => Criteria2Met(p.ToString()));
+
+            Console.WriteLine(criteria1Met);
+            Console.WriteLine(criteria2Met);
+            Console.ReadKey(true);
+        }
+
+        private static bool Criteria1Met(string password)
+        {
+            return password.GroupBy(c => c).Any(g => g.Count() >= 2) && NeverDecreases(password);
+        }
+
+        private static bool Criteria2Met(string password)
+        {
+            return password.GroupBy(c => c).Any(g => g.Count() == 2) && NeverDecreases(password);
+        }
+
+        private static bool NeverDecreases(string password)
+        {
+            var prev = password[0];
+
+            for (var i = 1; i < 6; i++)
+            {
+                if (password[i] < prev)
+                {
+                    return false;
+                }
+
+                prev = password[i];
+            }
+
+            return true;
+        }
+    }
+}

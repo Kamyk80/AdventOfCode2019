b9b0648009a689d9ac266a2e2eae4f4e808fd8c7
Kamyk80 <kamyk80@outlook.com>
2019-12-09 11:03:20 +0100

    Day 09

---
+++ AdventOfCode2019/AdventOfCode2019.Day09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Day09
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var program = File.ReadAllText("input.txt")
+                .Split(',')
+                .Select(long.Parse)
+                .ToList();
+
+            Console.WriteLine(string.Join(',', new Computer(program).Run(1)));
+            Console.WriteLine(string.Join(',', new Computer(program).Run(2)));
+            Console.ReadKey(true);
+        }
+    }
+}

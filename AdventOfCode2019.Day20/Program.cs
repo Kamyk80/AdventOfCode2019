a1c70506adca343aeb2381b1e776b3d8e3402221
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2019-12-21 00:15:21 +0100

    Day 20 part 2

--- AdventOfCode2019.Day20/Program.cs
+++ AdventOfCode2019.Day20/Program.cs
@@ -16,16 +16,27 @@ namespace AdventOfCode2019.Day20
             _maze = File.ReadAllLines("input.txt")
                 .Select(l => l.ToArray())
                 .ToArray();
+ 
+            var entrance = FindEntrance();
 
-            Console.WriteLine(ShortestPath());
+            Console.WriteLine(PathPortals(entrance));
+            Console.WriteLine(PathRecursive(entrance));
             Console.ReadKey(true);
         }
 
-        private static int ShortestPath()
+        private static (int x, int y) FindEntrance() =>
+            Enumerable.Range(2, _maze.Length - 4)
+                .SelectMany(y => Enumerable.Range(2, _maze[0].Length - 4), (y, x) => (x, y))
+                .Where(c => _maze[c.y][c.x] == '.')
+                .First(c => _maze[c.y][c.x - 2] == 'A' && _maze[c.y][c.x - 1] == 'A' ||
+                            _maze[c.y][c.x + 1] == 'A' && _maze[c.y][c.x + 2] == 'A' ||
+                            _maze[c.y - 2][c.x] == 'A' && _maze[c.y - 1][c.x] == 'A' ||
+                            _maze[c.y + 1][c.x] == 'A' && _maze[c.y + 2][c.x] == 'A');
+
+        private static int PathPortals((int x, int y) entrance)
         {
-            var start = FindPortals(new[] {'A', 'A'}).First();
-            var nodes = new Queue<(int x, int y, int time)>(new[] {(start.x, start.y, 0)});
-            var visited = new HashSet<(int x, int y)>(new[] {(start.x, start.y)});
+            var nodes = new Queue<(int x, int y, int time)>(new[] {(entrance.x, entrance.y, 0)});
+            var visited = new HashSet<(int x, int y)>(new[] {(entrance.x, entrance.y)});
 
             while (nodes.Count > 0)
             {
@@ -43,31 +54,19 @@ namespace AdventOfCode2019.Day20
                     visited.Add((move.x, move.y));
                 }
 
-                var portals = new[]
-                    {
-                        new[] {_maze[node.y - 2][node.x], _maze[node.y - 1][node.x]},
-                        new[] {_maze[node.y + 1][node.x], _maze[node.y + 2][node.x]},
-                        new[] {_maze[node.y][node.x - 2], _maze[node.y][node.x - 1]},
-                        new[] {_maze[node.y][node.x + 1], _maze[node.y][node.x + 2]}
-                    }
-                    .Where(p => p.All(c => c >= 'A' && c <= 'Z'))
-                    .ToList();
+                var portal = FindPortal((node.x, node.y));
 
-                foreach (var portal in portals)
+                if (portal.HasValue)
                 {
-                    if (portal[0] == 'Z' && portal[1] == 'Z')
+                    if (portal.Value == (0, 0))
                     {
                         return node.time;
                     }
 
-                    var exits = FindPortals(portal)
-                        .Where(p => !visited.Contains((p.x, p.y)))
-                        .ToList();
-
-                    foreach (var exit in exits)
+                    if (!visited.Contains(portal.Value))
                     {
-                        nodes.Enqueue((exit.x, exit.y, node.time + 1));
-                        visited.Add((exit.x, exit.y));
+                        nodes.Enqueue((portal.Value.x, portal.Value.y, node.time + 1));
+                        visited.Add((portal.Value.x, portal.Value.y));
                     }
                 }
             }
@@ -75,14 +74,135 @@ namespace AdventOfCode2019.Day20
             throw new Exception();
         }
 
-        private static IEnumerable<(int x, int y)> FindPortals(char[] portal) =>
-            Enumerable.Range(2, _maze.Length - 4)
+        private static (int x, int y)? FindPortal((int x, int y) tile)
+        {
+            var portal = new[]
+                {
+                    new[] {_maze[tile.y - 2][tile.x], _maze[tile.y - 1][tile.x]},
+                    new[] {_maze[tile.y + 1][tile.x], _maze[tile.y + 2][tile.x]},
+                    new[] {_maze[tile.y][tile.x - 2], _maze[tile.y][tile.x - 1]},
+                    new[] {_maze[tile.y][tile.x + 1], _maze[tile.y][tile.x + 2]}
+                }
+                .FirstOrDefault(p => p.All(c => c >= 'A' && c <= 'Z'));
+
+            if (portal == null)
+            {
+                return null;
+            }
+
+            if (portal[0] == 'A' && portal[1] == 'A')
+            {
+                return null;
+            }
+
+            if (portal[0] == 'Z' && portal[1] == 'Z')
+            {
+                return (0, 0);
+            }
+
+            return Enumerable
+                .Range(2, _maze.Length - 4)
                 .SelectMany(y => Enumerable.Range(2, _maze[0].Length - 4), (y, x) => (x, y))
                 .Where(c => _maze[c.y][c.x] == '.')
-                .Where(c => _maze[c.y][c.x - 2] == portal[0] && _maze[c.y][c.x - 1] == portal[1] ||
+                .Where(c => c != tile)
+                .First(c => _maze[c.y][c.x - 2] == portal[0] && _maze[c.y][c.x - 1] == portal[1] ||
                             _maze[c.y][c.x + 1] == portal[0] && _maze[c.y][c.x + 2] == portal[1] ||
                             _maze[c.y - 2][c.x] == portal[0] && _maze[c.y - 1][c.x] == portal[1] ||
-                            _maze[c.y + 1][c.x] == portal[0] && _maze[c.y + 2][c.x] == portal[1])
-                .ToList();
+                            _maze[c.y + 1][c.x] == portal[0] && _maze[c.y + 2][c.x] == portal[1]);
+        }
+
+        private static int PathRecursive((int x, int y) entrance)
+        {
+            var start = FindEntrance();
+            var nodes = new Queue<(int x, int y, int level, int time)>(new[] {(start.x, start.y, 0, 0)});
+            var visited = new HashSet<(int x, int y, int level)>(new[] {(start.x, start.y, 0)});
+
+            while (nodes.Count > 0)
+            {
+                var node = nodes.Dequeue();
+
+                var moves = Offsets
+                    .Select(o => (x: node.x + o.dx, y: node.y + o.dy))
+                    .Where(p => _maze[p.y][p.x] == '.')
+                    .Where(p => !visited.Contains((p.x, p.y, node.level)))
+                    .ToList();
+
+                foreach (var move in moves)
+                {
+                    nodes.Enqueue((move.x, move.y, node.level, node.time + 1));
+                    visited.Add((move.x, move.y, node.level));
+                }
+
+                var portal = FindPortal((node.x, node.y, node.level));
+
+                if (portal.HasValue)
+                {
+                    if (portal.Value == (0, 0, 0))
+                    {
+                        return node.time;
+                    }
+
+                    if (!visited.Contains(portal.Value))
+                    {
+                        nodes.Enqueue((portal.Value.x, portal.Value.y, portal.Value.level, node.time + 1));
+                        visited.Add((portal.Value.x, portal.Value.y, portal.Value.level));
+                    }
+                }
+            }
+
+            throw new Exception();
+        }
+
+        private static (int x, int y, int level)? FindPortal((int x, int y, int level) tile)
+        {
+            var portal = new[]
+                {
+                    new[] {_maze[tile.y - 2][tile.x], _maze[tile.y - 1][tile.x]},
+                    new[] {_maze[tile.y + 1][tile.x], _maze[tile.y + 2][tile.x]},
+                    new[] {_maze[tile.y][tile.x - 2], _maze[tile.y][tile.x - 1]},
+                    new[] {_maze[tile.y][tile.x + 1], _maze[tile.y][tile.x + 2]}
+                }
+                .FirstOrDefault(p => p.All(c => c >= 'A' && c <= 'Z'));
+
+            if (portal == null)
+            {
+                return null;
+            }
+
+            if (portal[0] == 'A' && portal[1] == 'A')
+            {
+                return null;
+            }
+
+            if (portal[0] == 'Z' && portal[1] == 'Z')
+            {
+                if (tile.level == 0)
+                {
+                    return (0, 0, 0);
+                }
+
+                return null;
+            }
+
+            var outer = tile.y == 2 || tile.x == 2 || tile.y == _maze.Length - 3 || tile.x == _maze[0].Length - 3;
+
+            if (tile.level == 0 && outer)
+            {
+                return null;
+            }
+
+            var level = tile.level + (outer ? -1 : 1);
+
+            return Enumerable
+                .Range(2, _maze.Length - 4)
+                .SelectMany(y => Enumerable.Range(2, _maze[0].Length - 4), (y, x) => (x, y))
+                .Where(c => _maze[c.y][c.x] == '.')
+                .Where(c => c.x != tile.x && c.y != tile.y)
+                .Select(c => (c.x, c.y, level))
+                .First(c => _maze[c.y][c.x - 2] == portal[0] && _maze[c.y][c.x - 1] == portal[1] ||
+                            _maze[c.y][c.x + 1] == portal[0] && _maze[c.y][c.x + 2] == portal[1] ||
+                            _maze[c.y - 2][c.x] == portal[0] && _maze[c.y - 1][c.x] == portal[1] ||
+                            _maze[c.y + 1][c.x] == portal[0] && _maze[c.y + 2][c.x] == portal[1]);
+        }
     }
 }

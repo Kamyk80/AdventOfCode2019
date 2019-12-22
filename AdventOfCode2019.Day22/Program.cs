a55219839bd13a7f24361a61df98398bf581fc49
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2019-12-23 00:15:12 +0100

    Day 22 part 2

--- AdventOfCode2019.Day22/Program.cs
+++ AdventOfCode2019.Day22/Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 
 #pragma warning disable 8509
 namespace AdventOfCode2019.Day22
 {
     internal class Program
     {
+        private const int SmallDeck = 10007;
+        private const long LargeDeck = 119315717514047;
+        private const long Repeats = 101741582076661;
+
         private static void Main()
         {
             var lines = File.ReadAllLines("input.txt");
 
             Console.WriteLine(Position(lines));
+            Console.WriteLine(Card(lines));
             Console.ReadKey(true);
         }
 
         private static int Position(string[] lines) =>
-            lines.Aggregate(Enumerable.Range(0, 10007).ToArray(), (deck, line) => line switch
+            lines.Aggregate(Enumerable.Range(0, SmallDeck).ToArray(), (deck, line) => line switch
                 {
                     var l when l.Equals("deal into new stack") => DealIntoNewStack(deck),
                     var l when l.StartsWith("cut ") => CutCards(deck, int.Parse(l.Substring(4))),
@@ -43,6 +49,44 @@ namespace AdventOfCode2019.Day22
                 .OrderBy(p => p.i * value % deck.Length)
                 .Select(p => p.c)
                 .ToArray();
+
+        private static BigInteger Card(string[] lines)
+        {
+            var incrementOne = BigInteger.One;
+            var offsetOne = BigInteger.Zero;
+
+            foreach (var line in lines)
+            {
+                if (line.Equals("deal into new stack"))
+                {
+                    incrementOne *= -1;
+                    incrementOne %= LargeDeck;
+                    offsetOne += incrementOne;
+                    offsetOne %= LargeDeck;
+                }
+                else if (line.StartsWith("cut "))
+                {
+                    var value = int.Parse(line.Substring(4));
+                    offsetOne += incrementOne * value;
+                    offsetOne %= LargeDeck;
+                }
+                else if (line.StartsWith("deal with increment "))
+                {
+                    var value = int.Parse(line.Substring(20));
+                    incrementOne *= ModularInverse(value);
+                    incrementOne %= LargeDeck;
+                }
+            }
+
+            var incrementAll = BigInteger.ModPow(incrementOne, Repeats, LargeDeck);
+            var offsetAll = offsetOne * (1 - incrementAll) * ModularInverse((1 - incrementOne) % LargeDeck);
+            offsetAll %= LargeDeck;
+
+            return (offsetAll + 2020 * incrementAll) % LargeDeck;
+        }
+
+        private static BigInteger ModularInverse(BigInteger value) =>
+            BigInteger.ModPow(value, LargeDeck - 2, LargeDeck);
     }
 }
 #pragma warning restore 8509
---
+++ AdventOfCode2019.Day22/readme.txt
@@ -0,0 +1,2 @@
+This puzzle completely beat me. Solution of part 2 isn't mine, it's based on this article:
+https://www.reddit.com/r/adventofcode/comments/ee0rqi/2019_day_22_solutions/fbnkaju/
